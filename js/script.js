09ad4393cf9bd75ad408fb20f8a030c2b77757bb
ramasrini1 <ramasrini1@gmail.com>
2021-02-17 20:43:04 -0800

    My First Assignment 01

---
+++ js/script.js
@@ -0,0 +1,123 @@
+//Name: Rama Srinivas 
+
+//Step 1
+//let someMonth;
+//function theMonth(){}
+//let currentMonth;
+//let summerMonth;
+//let MyLibraryFunction;
+
+//Step 2
+//3.9         Numeric Literal
+//"Hello!"    String Literal
+//false       Boolean Literal
+//null        Literal null value
+
+//Step 3
+//let anExpression = 3 * (4 / 5) + 6;
+//let anotherExpression = (4 * (8/4)) + (12%2);
+//console.log(anExpression);
+//console.log(anotherExpression);
+
+//Step 4
+//var firstName;
+//var lastName;
+//var address;
+//var city;
+//var state;
+//var zipCode;
+//var yourAge;
+//var referralSource;
+//var mayWeContactYou;
+
+//Step 5
+//var firstName = "Rama";
+//let lastName = "Srinivas";
+//const state = "CA";
+
+//Step 6
+//var temp = 2 + " apples";
+//console.log("testing coercing num and String " + temp);
+
+//Step 7
+//var boolAndString;
+//var numAndBool;
+//boolAndString = true + " Values";
+//numAndBool = 2 + true;
+
+//console.log("testing coercing boolean and String " + boolAndString);
+//console.log("testing coercing number and boolean " + numAndBool);
+
+//Step 8 Use escape character before double quotes to make it a vailid string
+//let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+//window.console.log(someString);
+
+//Step 9
+//let x = null;
+//console.log("This should display null " + x);
+//let undefinedValue;
+//console.log("This should display undefined value " + undefinedValue);
+
+//Step 10
+//let car = new Object();
+//let counter;
+//console.log("Testing typeOf for string hello: " + typeof("hello"));
+//console.log("Testing typeOf for number 3: " + typeof(3));
+//console.log("Testing typeOf for boolean value true: " + typeof(true));
+//console.log("Testing typeOf for Object named car: " + typeof(car));
+//console.log("Testing typeOf for unassigned variable:  " + typeof(counter));
+
+//Step 11
+//alert("Hello " +  "Rama Srinivas" + ", welcome to the JavaScript class!");
+
+//Step 12 repalcing with variable name
+//let name = "Rama Srinivas";
+//alert("Hello " + name + ", welcome to the JavaScript class!");
+
+//Step 13 replacing with variable course
+//let course ="JavaScript";
+//let name = "Rama Srinivas";
+//alert("Hello " + name + ", welcome to the " + course + " class!");
+
+//Step 14 Adding \n
+//alert("Hello " + name + ".\n Welcome to the " + course + " class!");
+
+//Step 15
+//let name = prompt("Enter Your Full Name", "First Name Last Name");
+//let course ="JavaScript";
+//alert(`Hello ${name}.\n Welcome to the ${course} class!`);
+
+//Step 16
+//let name = prompt("Enter Your Full Name", "Tom Hanks");
+//let course = prompt("Enter Your Course Name", "JavaScript");
+//alert(`Hello ${name}.\n Welcome to the ${course} class!`);
+
+//Step 17
+//let x = 10;
+//let y = 20;
+//console.log(x+y);
+
+//Step 18
+//let x = 20;
+//x += 20;
+//console.log(x);
+
+//Step 19
+//let x = 20;
+//x *= 5;
+//console.log(x);
+
+//Step 20
+//let x = 20 % 3;
+//x /= 1;
+//console.log(x);
+
+//Step 21
+//let x = 5;
+//let y = 5;
+//console.log(x==y);
+
+//Step 22
+//let x = 5;
+//let y = 5;
+//console.log(x>y);
